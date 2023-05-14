e8fc7f4ee63e322a7c12dbc71c69af471ac53af2
rubik776 <gabriel.izurieta@epn.edu.ec>
2023-05-13 19:29:46 -0500

    Curso de React
    Gaaaaa- aprendo react

---
+++ Curses/React/mi-App01/mi-app/src/componentes/Testimonio.js
@@ -0,0 +1,26 @@
+import REACT from 'react';
+import '../hojas-de-estilo/Testimonio.css'
+
+function Testimonio(promps){
+    return(
+        <div className='contenedor-testimonio'>                    
+           
+            <img className='imagen-testimonio'
+            /*src={/*require(`../imagenes/testimonio-${promps.imagen}.png`)}*/
+            src={require(`../imagenes/testimonio-${promps.imagen}.png`)}
+            alt='Foto de Emma'/>
+            
+            <div className='contenedor-texto-testimonio'>
+            
+            {/* Para resaltar el testo puedo usar <strong>*/}
+                <p className='nombre-testimonio'><strong>{promps.nombre}</strong> en {promps.pais}</p>
+
+                <p className='cargo-testimonio'>{promps.cargo} en <b>{promps.empresa}</b></p>
+
+                <p className='texto-testimonio'>"{promps.testimonio}".</p>
+            </div>
+        </div>
+    );
+}
+
+export default Testimonio;
